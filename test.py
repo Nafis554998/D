ba9dcdc8418f6817bc263fe0415aa0e2631a4d71
Nafis554998 <142597957+Nafis554998@users.noreply.github.com>
2024-03-13 20:27:01 +0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,373 @@
+import os
+import httpx
+from os import system as osRUB
+from os import system as cmd
+import os,time,platform
+import os, platform, time, sys
+#os.system('clear')
+#print(' [\x1b[38;5;48m●\033[1;37m]\x1b[38;5;46m CHECKING UPDATE...!\033[1;37m [\x1b[38;5;48m–\033[1;37m]\033[1;37m')
+time.sleep(2)
+os.system('clear')
+print('\033[1;97m[\x1b[38;5;48m●\033[1;37m] \x1b[38;5;50mJOIN MY PUBLIC GROUP ')
+time.sleep(0.1)
+os.system(f'xdg-open https://m.facebook.com/groups/636462365122965/?ref=share&mibextid=NSMWBT')
+os.system('clear')
+print('\x1b[38;5;46mLoding...\x1b[38;5;196m[\x1b[38;5;46mTEAM-69\x1b[38;5;196m]\x1b[38;5;46m')
+time.sleep(2)
+try:
+    import requests 
+except ImportError:
+    print('\n  installing Requests ...\n')
+    os.system('pip install requests')
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n  installing futures ...\n')
+    os.system('pip install futures')
+try:
+    import mechanize
+except ModuleNotFoundError:
+    os.system('pip install mechanize > /dev/null')
+
+os.system('git pull');os.system('termux-setup-storage')
+try:
+    import requests
+except ImportError:
+    print('\n \033[1;91m[\033[1;93mTEAM-69\033[1;91m]\033[1;97m installing bs4 !...\n')
+    time.sleep(0.5)
+    os.system('pip install requests')
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n \033[1;91m[\033[1;93mTEAM-69\033[1;91m]\033[1;97m installing futures !...\n')
+    time.sleep(0.5)
+    os.system('pip install futures')
+
+try:
+    import bs4
+except ImportError:
+    print('\n \033[1;91m[\033[1;93mTEAM-69\033[1;91m]\033[1;97m installing bs4 !...\n')
+    time.sleep(0.5)
+    os.system('pip install bs4')
+
+import os, platform, time, sys
+
+try:
+ import requests
+except:os.system("pip uninstall requests -y;pip install requests")
+
+print('\033[1;91m[\033[1;97m-\033[1;91m] \033[1;92mChecking For Update. . . .')
+#os.system('espeak -a 300 " Waiting for Update,"')
+time.sleep(2)
+
+
+os.system('git pull --quiet 2>/dev/null')
+bit = platform.architecture()[0]
+if bit == '64bit':
+ print('\033[1;91m[\033[1;92m◉\033[1;91m] \033[1;92mYOU ARE 64BIT USER')
+ 
+elif bit == '32bit':
+ print('\033[1;91m[\033[1;92m◉\033[1;91m] \033[1;92mYOU ARE 32BIT USER')
+
+def fucker():        
+    os.system('rm -rf /sdcard/HENTAI')
+try:
+    mursalin = open("/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/models.py", "r").read()
+    alin = len(mursalin)
+    if alin != 35223:
+        fucker()
+        exit(f'\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mFUCK BYPASS USER... ')
+    else:
+        pass 
+except:
+    fucker()
+    exit(f'\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mFUCK BYPASS USER... ')
+try:
+    mursalin = open("/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/auth.py", "r").read()
+    alin = len(mursalin)
+    if alin != 10187:
+        fucker()
+        exit(f'\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mFUCK BYPASS USER... ')
+    else:
+        pass 
+except:
+    fucker()
+    exit(f'\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mFUCK BYPASS USER... ')
+try:
+    mursalin = open("/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/api.py", "r").read()
+    alin = len(mursalin)
+    if alin != 6449:
+        fucker()
+        exit(f'\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mFUCK BYPASS USER... ')
+    else:
+        pass 
+except:
+    fucker()
+    exit(f'\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mFUCK BYPASS USER... ')
+try:
+    mursalin = open("/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/sessions.py", "r").read()
+    alin = len(mursalin)
+    if alin != 30373:
+        fucker()
+        exit(f'\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mFUCK BYPASS USER... ')
+    else:
+        pass 
+except:
+    fucker()
+    exit(f'\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mFUCK BYPASS USER... ')
+try:
+    king='/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/'
+    if not 'print' in open(king+'sessions.py','r').read():
+        pass
+    else:
+        fucker()
+        exit(f'\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mFUCK BYPASS USER... ')
+except:
+    fucker()
+    exit(f'\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mFUCK BYPASS USER... ')
+try:
+    qeen='/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/'
+    if not 'print' in open(qeen+'models.py','r').read():
+        pass
+    else:
+        fucker()
+        exit(f'\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mFUCK BYPASS USER... ')
+except:
+    fucker()
+    exit(f'\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mFUCK BYPASS USER... ')
+try:
+    don='/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/'
+    if not 'print' in open(don+'api.py','r').read():
+        pass
+    else:
+        fucker()
+        exit(f'\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mFUCK BYPASS USER... ')
+except:
+    fucker()
+    exit(f'\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mFUCK BYPASS USER... ')
+try:
+    king='/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/'
+    if not 'sys.stdout.write' in open(king+'sessions.py','r').read():
+        pass
+    else:
+        fucker()
+        exit(f'\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mFUCK BYPASS USER... ')
+except:
+    fucker()
+    exit(f'\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mFUCK BYPASS USER... ')
+try:
+    qeen='/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/'
+    if not 'sys.stdout.write' in open(qeen+'models.py','r').read():
+        pass
+    else:
+        fucker()
+        exit(f'\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mFUCK BYPASS USER... ')
+except:
+    fucker()
+    exit(f'\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mFUCK BYPASS USER... ')
+try:
+    don='/data/data/com.termux/files/usr/lib/python3.11/site-packages/requests/'
+    if not 'sys.stdout.write' in open(don+'api.py','r').read():
+        pass
+    else:
+        fucker()
+        exit(f'\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mFUCK BYPASS USER... ')
+except:
+    fucker()
+    exit(f'\x1b[38;5;196m[\x1b[1;97m●\x1b[38;5;196m] \x1b[38;5;46mFUCK BYPASS USER... ')
+    
+from urllib.request import Request, urlopen
+import os, requests, re,platform, sys, random, subprocess, threading, itertools,base64,uuid,zlib,re,json,uuid,subprocess,shutil,webbrowser,time,json,sys,random,datetime,time,re,subprocess,platform,string,json,time,re,random,sys,string,uuid
+from concurrent.futures import ThreadPoolExecutor as NAFISNAFIS
+from string import * 
+from random import randint
+from time import sleep as slp
+from os import system as cmd
+from zlib import decompress 
+import os, platform
+from concurrent.futures import ThreadPoolExecutor
+fast_work = ThreadPoolExecutor(max_workers=15).submit 
+model2 = requests.get('https://facebook.com/groups/1147370869269844//').text.splitlines()
+totaldmp = 0
+count = 0
+loop = 0
+oks = []
+cps = []
+id = []
+ps = []
+sid = []
+total=[]
+methods = []
+srange = 0
+saved = []
+totaldmp = 0
+filter = []    
+sim_id = ''
+android_version = subprocess.check_output('getprop ro.build.version.release',shell=True).decode('utf-8').replace('\n','')
+model = subprocess.check_output('getprop ro.product.model',shell=True).decode('utf-8').replace('\n','')
+build = subprocess.check_output('getprop ro.build.id',shell=True).decode('utf-8').replace('\n','')
+fblc = 'en_US'
+try:
+        fbcr = subprocess.check_output('getprop gsm.operator.alpha',shell=True).decode('utf-8').split(',')[0].replace('\n','')
+except:
+        fbcr = 'Zong'
+fbmf = subprocess.check_output('getprop ro.product.manufacturer',shell=True).decode('utf-8').replace('\n','')
+fbbd = subprocess.check_output('getprop ro.product.brand',shell=True).decode('utf-8').replace('\n','')
+fbdv = model
+fbsv = android_version
+fbca = subprocess.check_output('getprop ro.product.cpu.abilist',shell=True).decode('utf-8').replace(',',':').replace('\n','')
+fbdm = '{density=2.0,height='+subprocess.check_output('getprop ro.hwui.text_large_cache_height',shell=True).decode('utf-8').replace('\n','')+',width='+subprocess.check_output('getprop ro.hwui.text_large_cache_width',shell=True).decode('utf-8').replace('\n','')
+try:
+        fbcr = subprocess.check_output('getprop gsm.operator.alpha',shell=True).decode('utf-8').split(',')
+        total = 0
+        for i in fbcr:
+                total+=1
+        select = ('1','2')
+        if select == '1':
+                fbcr = subprocess.check_output('getprop gsm.operator.alpha',shell=True).decode('utf-8').split(',')[0].replace('\n','')
+                sim_id+=fbcr
+        elif select == '2':
+                try:
+                        fbcr = subprocess.check_output('getprop gsm.operator.alpha',shell=True).decode('utf-8').split(',')[1].replace('\n','')
+                        sim_id+=fbcr
+                except Exception as e:
+                        fbcr = "Zong"
+                        sim_id+=fbcr
+        else:
+                fbcr = 'Zong'
+                sim_id+=fbcr
+except:
+        fbcr = "Zong"
+device = {
+        'android_version':android_version,
+        'model':model,
+        'build':build,
+        'fblc':fblc,
+        'fbmf':fbmf,
+        'fbbd':fbbd,
+        'fbdv':model,
+        'fbsv':fbsv,
+        'fbca':fbca,
+        'fbdm':fbdm}
+
+
+S = '\033[1;37m'
+A = '\x1b[38;5;208m'
+R = '\x1b[38;5;46m'
+F = '\x1b[38;5;48m'
+Z = '\033[1;33m'
+yellow = "\x1b[38;5;208m"
+black="\033[1;30m"
+rad="\033[1;31m"
+green="\x1b[38;5;47m"
+yelloww="\033[1;33m"
+blue="\033[1;34m"
+purple="\033[1;35m"
+cyan="\033[1;36m"
+white="\033[1;37m"
+faltu = "\033[1;41m"
+pvt = "\033[1;0m"
+my_color = [white,blue,green]
+warna = random.choice(my_color)
+
+loop=0
+oks=[]
+cps=[]
+pcp=[]
+id=[]
+tokenku=[]
+#+++++++++++M1-USERAGENT+++++++++++#
+
+    
+    
+sys.stdout.write('\x1b]2; =[🌺]=[TEAM-69]=[🌺]=\x07')
+S = '\033[1;97m'
+A = '\x1b[38;5;208m'
+R = '\x1b[38;5;46m'
+F = ' '
+Z = '\033[1;33m'
+head = {'Host': 'adsmanager.facebook.com', 'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"', 'viewport-width': '980'}
+#+++++++++++LOGO+++++++++++#
+logo=(f'''
+
+\x1b[38;5;196m [\x1b[38;5;46m=\x1b[38;5;196m]\x1b[38;5;51m VERSION \033[1;97m   ● \033[1;97mV\x1b[38;5;46m/\033[1;97m0.11
+\033[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━''')
+#================OK-CP-RESULT==============#    
+def result(OKs,cps):
+    if len(OKs) != 0 or len(cps) != 0:
+        print('')
+        print(f'\033[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+        print('\033[1;97m[\x1b[38;5;47m≈\033[1;97m] \033[1;97mTHE PROGRESS HAS BEEN COMPLETE ')
+        print('\033[1;97m[\x1b[38;5;47m≈\033[1;97m] \033[1;97mTOTAL OK ID:\x1b[38;5;47m %s' % str(len(oks)))
+        print('\033[1;97m[\x1b[38;5;47m≈\033[1;97m] \033[1;97mTOTAL CP ID:\033[1;30m %s' % str(len(cps)))
+        print(f'\033[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+        input("Press enter to back NAFIS Menu ")
+        exit()
+#++++++++++++++++++MAIN-MENU++++++++++++++#
+def NAFIS():   
+    os.system('clear')
+    print(logo)
+    print("\x1b[38;5;196m[\x1b[38;5;49m1\x1b[38;5;196m]\x1b[38;5;46m FILE CLONING\033[1;37m")
+    print("\x1b[38;5;196m[\x1b[38;5;49m2\x1b[38;5;196m]\x1b[38;5;48m CONTACT ADMIN\033[1;37m")
+    print("\x1b[38;5;196m[\x1b[38;5;49m0\x1b[38;5;196m]\x1b[38;5;49m EXIT")
+    print(f'\033[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+    select = input('Select Menu :\033[1;35m ')
+    if select =='1':
+        method_crack()
+    elif select =='2':
+        os.system(f'xdg-open https://m.me/j/AbZzndZFDLI6K7Dr/')
+        NAFIS()        
+    elif select =='0':
+        exit()
+    else:
+        print('\x1b[38;5;46m Select Valid Option ...')
+    time.sleep(2)
+#+++++++++++++++METHOD-MENU++++++++++++#        
+def method_crack():
+    global methods
+    os.system('clear')
+    print(logo)
+    print(f'\x1b[38;5;196m[\x1b[38;5;46m≈\x1b[38;5;196m]\x1b[38;5;46m METHOD  ❯ 1💚')
+    print(f'\x1b[38;5;196m[\x1b[38;5;46m≈\x1b[38;5;196m]\x1b[38;5;46m METHOD show cookies ❯ 2💚')
+    print(f'\x1b[38;5;196m[\x1b[38;5;46m≈\x1b[38;5;196m]\x1b[38;5;46m METHOD  CP OFF❯ 3💚')
+    print(f'\033[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+    option = input('Select method:\033[1;97m ')
+    if option =='1':
+        methods.append('methodA')
+        main_crack().crack(id)
+    elif option =='2':
+        methods.append('methodB')
+        main_crack().crack(id)
+    elif option =='3':
+        methods.append('methodC')
+        main_crack().crack(id)
+    else:
+      print('\n Select Valid Option ...')
+      time.sleep(2)
+      method_crack()
+
+#++++++++++++±+++++METHOD-PASW-MENU+++++++++++++++++#            
+
+#++++++++LICENCES++++++++#
+
+import os,httpx
+def approval():
+    TEAM69="TEAM69+"
+    HENTAI="HENTAI"
+    uuid=str(os.getuid()) + str(os.getlogin())
+    key = "`".join(uuid)
+    ress=httpx.get("https://github.com/TEAM-69HENTAI/Apv/blob/main/Apv.txt").text
+    if key in ress:
+        method_crack()
+    else:
+        print("\033[1;97m[\033[1;32m+\033[1;97m] \033[1;32mKEY IS NOT APPROVED")
+        os.system("clear")
+        print(logo)
+        print("\033[1;97m[\033[1;32m+\033[1;97m] \033[1;32mPAID BUT FREE\033[1;97m ● \033[1;32mFOR FRIEND ")
+        print("\033[1;97m[\033[1;32m+\033[1;97m] \033[1;32mYour key :\x1b[1;96m "+TEAM69+HENTAI+key)
+        print(41*'\033[1;97m—')
+        #name = input("\033[1;97m[\033[1;97m≈\033[1;97m] \033[1;32mYOUR NAME : ")
+        os.system("xdg-open https://m.me/TEAM69.HENTAI")
+approval()
+        
+#++++++++++++++APPROVAL+++++++++++++++#
